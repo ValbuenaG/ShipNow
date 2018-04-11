87d6799d6bf6bd58e78f7bc59e8ff021b438f135
Gustavo Valbuena <valbuena52@gmail.com>
2018-04-11 17:55:28 -0300

    fix anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -3,14 +3,16 @@ a = ['horse', 'shore', 'heros', 'standard']
 
 def isAnagram(word, posibleAnagrams)
 	wordToArr = word.split('').sort
+	trueAnagrams = []
 	posibleAnagrams.length.times do |i|
 		aux = posibleAnagrams[i]
 		aux = aux.split('').sort
-		if(wordToArr[i] != aux[i])
-			posibleAnagrams.delete_at(i)
+		if(wordToArr[i] == aux[i] &&aux.length == wordToArr.length )
+			trueAnagrams.push(posibleAnagrams[i])
+		
 		end
 	end
-	posibleAnagrams
+	trueAnagrams
 end
 
 puts isAnagram('horse', a)
